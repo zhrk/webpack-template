dec9424d2d6f4a438bbe35464c4c1201ea1b20a0
Zakhar Shchipets <zshchipets@gmail.com>
2020-04-25 15:49:28 +0300

    fix root

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import styles from './styles.css';
 
-const Root = () => {
-  const test = 1;
-
-  const a = 2;
-
-  const jopa = (asd) => {
-    return a + asd;
-  };
-
-  return <div className={styles.container}>root</div>;
-};
+const Root = () => <div className={styles.container}>root</div>;
 
 export default Root;
