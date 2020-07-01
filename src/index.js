cea1227333eb59b4c4aecf97982cceecf1d67bba
Zakhar Shchipets <zshchipets@gmail.com>
2020-07-02 02:35:50 +0300

    fix file structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './Root';
+import Root from './components/Root';
 import './styles/global.css';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
