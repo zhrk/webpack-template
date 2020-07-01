a329a7df528308ece9915b4e1eab5580fcb34828
Zakhar Shchipets <zshchipets@gmail.com>
2020-07-02 02:27:11 +0300

    update styles

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import styles from './styles.css';
 
-const Root = () => <div className={styles.container}>root</div>;
+const Root = () => (
+  <div className={styles.container}>
+    <h1>Hello</h1>
+  </div>
+);
 
 export default Root;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
+import './styles/global.css';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
