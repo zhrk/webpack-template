d367270d46498c0662384083d7d67f75d86998dc
Zakhar Shchipets <zshchipets@gmail.com>
2020-07-02 02:56:54 +0300

    add test funcs

--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.css';
 
-const Root = () => (
-  <div className={styles.container}>
-    <h1>Hello</h1>
-  </div>
-);
+const Root = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => setIsOpen(!isOpen);
+
+  return (
+    <div className={styles.container}>
+      <button type="button" onClick={handleClick}>{isOpen ? 'Close' : 'Open'}</button>
+      <h1>
+        We are
+        {isOpen ? ' open' : ' closed'}
+      </h1>
+    </div>
+  );
+};
 
 export default Root;
